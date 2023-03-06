9dc46270f0799f7377384cc651d69d822559e185
iLvkii <76495748+Exlion823@users.noreply.github.com>
2023-03-06 13:10:30 +0000

    creating functions and testing some new things

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import platform
 
 # get the os type
 OS_TYPE = platform.system()
-if OS_TYPE == Darwin:
-    OS_TYPE = Mac
+if OS_TYPE == "Darwin":
+    OS_TYPE = "Mac"
 
--- functions.py
+++ functions.py
@@ -1,1 +1,8 @@
+import os
+import platform
 
+def get_username():
+    print(f"The current logged-in user is: {os.getlogin()}")
+
+def get_devicename():
+    print(f"The device name is: {platform.node()}")
