b6beebaa0de02c089e04ff69eab1ddec9d49ed75
Vbuuu <m.rohm1@outlook.de>
2023-03-06 12:28:22 +0100

    Cool loading animation (not) Working hopefully

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import colorama
+from random import random
+colorama.init(autoreset=True)
+
+colors = [colorama.Fore.BLUE, colorama.Fore.RED, colorama.Fore.WHITE, colorama.Fore.GREEN]
+
+print(random.choice(colors) + """
+ _____       __       ______                           
+|_   _|     / _|      | ___ \                          
+  | | _ __ | |_ ___   | |_/ /___  __ _ _ __   ___ _ __ 
+  | || '_ \|  _/ _ \  |    // _ \/ _` | '_ \ / _ \ '__|
+ _| || | | | || (_) | | |\ \  __/ (_| | |_) |  __/ |   
+ \___/_| |_|_| \___/  \_| \_\___|\__,_| .__/ \___|_|   
+                                      | |              
+                                      |_|              """)
