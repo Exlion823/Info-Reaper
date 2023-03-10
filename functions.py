00ea364583776a8a21a8ab2ae5e65b6c57e5d95c
iLvkii <76495748+Exlion823@users.noreply.github.com>
2023-03-10 19:55:37 +0100

    added new get_ram function

--- functions.py
+++ functions.py
@@ -1,8 +1,16 @@
 import os
+import psutil
 import platform
+from cpuinfo import get_cpu_info
 
 def get_username():
     print(f"The current logged-in user is: {os.getlogin()}")
 
 def get_devicename():
     print(f"The device name is: {platform.node()}")
+
+def get_cpu_info():
+    pass
+
+def get_ram():
+    print(f"Total RAM installed: {round(psutil.virtual_memory().total/1000000000, 2)} GB")
